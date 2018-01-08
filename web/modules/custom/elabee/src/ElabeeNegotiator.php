07c7d39bb684e03a860f03ddd751be232aec0558
el7cosmos <el@elabee.me>
2018-01-08 21:57:01 +0700

    Add custom module.

---
+++ web/modules/custom/elabee/src/ElabeeNegotiator.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\elabee;
+
+use Drupal\Core\Path\PathMatcherInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Theme\ThemeNegotiatorInterface;
+
+/**
+ * Class ElabeeNegotiator.
+ */
+class ElabeeNegotiator implements ThemeNegotiatorInterface {
+
+  /**
+   * @var \Drupal\Core\Path\PathMatcherInterface
+   */
+  protected $pathMatcher;
+
+  /**
+   * Constructs a new ElabeeNegotiator object.
+   *
+   * @param \Drupal\Core\Path\PathMatcherInterface $path_matcher
+   *   The path mathcer service.
+   */
+  public function __construct(PathMatcherInterface $path_matcher) {
+    $this->pathMatcher = $path_matcher;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(RouteMatchInterface $route_match) {
+    return $this->pathMatcher->isFrontPage();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function determineActiveTheme(RouteMatchInterface $route_match) {
+    return 'vibe';
+  }
+
+}
