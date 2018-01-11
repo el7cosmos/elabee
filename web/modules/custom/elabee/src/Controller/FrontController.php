cfd1ad2fe3294a24f807b313ad47939b7b31b936
el7cosmos <el@elabee.me>
2018-01-12 06:25:32 +0700

    Change front page.

---
+++ web/modules/custom/elabee/src/Controller/FrontController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\elabee\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Class FrontController.
+ */
+class FrontController extends ControllerBase {
+
+  /**
+   * Front.
+   */
+  public function front() {
+    return [];
+  }
+
+}
