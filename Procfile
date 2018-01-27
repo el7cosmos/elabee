f2d50993403c7b436db3a611f867849b3c457181
el7cosmos <el@elabee.me>
2018-01-27 22:25:18 +0700

    Drush 9, pgpass file.

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-release: drush heroku-release --root=web --yes
 web: heroku-php-nginx -C nginx_app.conf web
---
+++ scripts/composer/Heroku.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace DrupalProject\composer;
+
+use Composer\Script\Event;
+use Dotenv\Dotenv;
+use DrupalFinder\DrupalFinder;
+use InvalidArgumentException;
+use Symfony\Component\Filesystem\Filesystem;
+
+class Heroku {
+
+  public static function pgPassFile(Event $event) {
+    $fs = new Filesystem();
+    $finder = new DrupalFinder();
+    $finder->locateRoot(getcwd());
+    $root = $finder->getComposerRoot();
+
+    try {
+      $dotenv = new Dotenv($root);
+      $dotenv->load();
+    }
+    catch (InvalidArgumentException $exception) {
+    }
+
+    if (!$fs->exists($root . '/.pgpass') && $database_url = getenv('DATABASE_URL')) {
+      $connection = parse_url($database_url);
+      $pass = implode(':', [
+        $connection['host'],
+        $connection['port'],
+        basename($connection['path']),
+        $connection['user'],
+        $connection['pass'],
+      ]);
+      $fs->dumpFile($root . '/.pgpass', $pass);
+      $fs->chmod($root . '/.pgpass', 0600);
+
+      $event->getIO()->write('Create PostgreSQL password file');
+    }
+  }
+
+}
