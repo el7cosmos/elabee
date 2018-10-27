795b7621868c5114545d3b1f437123e70678cb08
Abi أب <el@elabee.me>
2018-10-28 01:05:08 +0700

    Update deprecated test

--- web/modules/custom/migrate_facebook_destination/tests/src/Unit/Form/FacebookSettingsFormTest.php
+++ web/modules/custom/migrate_facebook_destination/tests/src/Unit/Form/FacebookSettingsFormTest.php
@@ -4,9 +4,14 @@ namespace Drupal\Tests\migrate_facebook_destination\Unit\Form;
 
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\migrate_facebook_destination\Form\FacebookSettingsForm;
 use Drupal\Tests\UnitTestCase;
 
+/**
+ * @coversDefaultClass \Drupal\migrate_facebook_destination\Form\FacebookSettingsForm
+ * @covers ::<!public>
+ */
 class FacebookSettingsFormTest extends UnitTestCase {
 
   /**
@@ -19,36 +24,51 @@ class FacebookSettingsFormTest extends UnitTestCase {
    */
   protected $formState;
 
-  public function testGetFormId() {
+  /**
+   * @covers ::getFormId
+   */
+  public function testGetFormId(): void {
     self::assertEquals('facebook_settings_form', $this->formObject->getFormId());
   }
 
-  public function testBuildForm() {
+  /**
+   * @covers ::buildForm
+   */
+  public function testBuildForm(): void {
     $subset = ['app_id', 'app_secret', 'default_access_token'];
     self::assertArraySubset($subset, array_keys($this->formObject->buildForm([], $this->formState)));
   }
 
-  public function testValidateForm() {
+  /**
+   * @covers ::validateForm
+   */
+  public function testValidateForm(): void {
     $form = [];
     self::assertNull($this->formObject->validateForm($form, $this->formState));
   }
 
-  public function testSubmitForm() {
+  /**
+   * @covers ::submitForm
+   */
+  public function testSubmitForm(): void {
+    $this->formObject->setMessenger($this->prophesize(MessengerInterface::class)->reveal());
     $form = [];
     self::assertNull($this->formObject->submitForm($form, $this->formState));
   }
 
+  /**
+   * {@inheritdoc}
+   */
   protected function setUp() {
     parent::setUp();
 
+    /** @var \Drupal\Core\Config\ConfigFactoryInterface $config_factory */
     $config_factory = $this->getConfigFactoryStub([
       'migrate_facebook_destination.settings' => [],
     ]);
     /** @var \PHPUnit_Framework_MockObject_MockObject $config */
     $config = $config_factory->getEditable('migrate_facebook_destination.settings');
-    $config->expects(self::any())
-      ->method('set')
-      ->willReturnSelf();
+    $config->method('set')->willReturnSelf();
 
     $container = new ContainerBuilder();
     $container->set('config.factory', $config_factory);
@@ -57,20 +77,11 @@ class FacebookSettingsFormTest extends UnitTestCase {
 
     $this->formObject = FacebookSettingsForm::create($container);
     $this->formState = $this->createMock(FormStateInterface::class);
-    $this->formState->expects(self::any())
-      ->method('getValues')
-      ->willReturn([
-        'app_id' => 'app_id',
-        'app_secret' => 'app_secret',
-        'default_access_token' => 'default_access_token',
-      ]);
+    $this->formState->method('getValues')->willReturn([
+      'app_id' => 'app_id',
+      'app_secret' => 'app_secret',
+      'default_access_token' => 'default_access_token',
+    ]);
   }
 
-}
-
-namespace Drupal\Core\Form;
-
-if (!function_exists('drupal_set_message')) {
-  function drupal_set_message() {
-  }
 }
